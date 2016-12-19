b2fbf8d5190306fbc022e40bf56ae4bd7ef8398a
Tom Fyles <Tom Fyles>
2016-12-19 22:42:11 +0000

    mental

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    console.log("Fucking work");
+	console.log("Fucking work");
 
-	
+	function foo() {
+
+		$(".merry").velocity({ opacity: 0 }, { duration: 1000})
+			.velocity({ opacity: 1 }, { duration: 1000});
+
+			$(".ho").velocity({ opacity: 1 }, { duration: 1000})
+			.velocity({ opacity: 0 }, { duration: 1000});
+
+			$(".photo").velocity({  rotateZ: "+=360" }, { duration: 5000, easing: "linear", loop: true});
+
+		setTimeout(foo, 500);
+	}
+
+	foo();
 
 });
