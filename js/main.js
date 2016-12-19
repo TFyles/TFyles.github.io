1a9ba3d2777914fa84c390c4f66b6b579f6033dd
Tom Fyles <Tom Fyles>
2016-12-19 22:52:07 +0000

    use secure cdn

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
 			.velocity({ opacity: 0 }, { duration: 1000});
 
 			$(".photo").velocity({  rotateZ: "+=360" }, { duration: 5000, easing: "linear", loop: true});
+			$(".photo2").velocity({  rotateY: "+=360" }, { duration: 5000, easing: "linear", loop: true});
 
 		setTimeout(foo, 500);
 	}
